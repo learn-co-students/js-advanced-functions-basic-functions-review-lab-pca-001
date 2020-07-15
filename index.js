0ea0c83603adb78c5d490390eaa006824aef688a
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-15 16:56:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+// Function Declaration
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+// Function Expression
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(spice="*") {
+  return function(adjective="special") {
+    return `You are ${spice}${adjective}${spice}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b
+  }
+}
+
+function actionApplyer(integer, arrayoffunctions) {
+  for (const varfunction of arrayoffunctions) {
+    integer = varfunction(integer)
+  }
+  return integer
+}
+
