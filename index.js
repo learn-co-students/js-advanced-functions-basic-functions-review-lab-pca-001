fc01c22192c00a1a597f3b7c918dbcf54f433ba0
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-15 15:14:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(string="roller-skate") {
+  return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string="go to the office") {
+  return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(string="*") {
+  return function(param="special") {
+    return `You are ${string}${param}${string}!`
+  }
+}
+
+const Calculator = {
+  add : function(num1, num2) {
+    return num1 + num2
+  },
+  subtract : function(num1, num2) {
+    return num1 - num2
+  },
+  multiply : function(num1, num2) {
+    return num1 * num2
+  },
+  divide : function(num1, num2) {
+    return num1 / num2
+  }
+}
+
+function actionApplyer(i, arr) {
+  if (arr.length) {
+    for (const func of arr) {
+      i = func(i)
+    }
+  }
+  return i
+}
