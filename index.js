616dc3ff450cd74f6298ef5ae4d197681ea085b7
Idan <idan.zonshein@powerhrg.com>
2020-07-16 16:17:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(obj = "*") {
+  return function (msg = "special") {
+    return `You are ${obj}${msg}${obj}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+function actionApplyer(start_int, func_array) {
+  let a = start_int
+
+  for (let i = 0; i < func_array.length; i++) {
+    a = func_array[i](a)
+  }
+  return a
+}
